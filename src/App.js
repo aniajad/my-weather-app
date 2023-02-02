6039ed78e3f2c9f89ac40e84b2773c418ca10145
aniajad <116589679+aniajad@users.noreply.github.com>
2023-02-02 22:13:36 +0100

    Added footer with GitHub repo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <footer>
+        Open Source on{" "}
+        <a
+          href="https://github.com/aniajad/my-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        by Anna JAdczyszyn
+      </footer>
+    </div>
+  );
 }
 
 export default App;
