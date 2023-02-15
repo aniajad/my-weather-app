615b0bbef0506c01ac728c67aa78a0e86ff21c1e
aniajad <116589679+aniajad@users.noreply.github.com>
2023-02-15 21:12:29 +0100

    Handling the refresh of forecast with useEffect, update on units and css

--- src/FeelsLike.js
+++ src/FeelsLike.js
@@ -1,42 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
+import "./FeelsLike.css";
 
 export default function FeelsLike(props) {
-  const [unit, setUnit] = useState("celsius");
-
-  function convertFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function convertCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div className="FeelsLike">
-        <span className="degrees">Feels like: {Math.round(props.celsius)}</span>
-        <span className="units">
-          °C|{" "}
-          <a href="/" onClick={convertFahrenheit}>
-            °F
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    let fahrenheit = (props.celsius * 9) / 5 + 32;
-    return (
-      <div className="FeelsLike">
-        <span>Feels like: {Math.round(fahrenheit)}</span>
-        <span className="units">
-          <a href="/" onClick={convertCelsius}>
-            °C
-          </a>
-          | °F
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="FeelsLike">
+      <span className="degrees">Feels like: {Math.round(props.celsius)}</span>
+      <span className="units">°C</span>
+    </div>
+  );
 }
--- src/TemperatureUnits.js
+++ src/TemperatureUnits.js
@@ -1,43 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import "./TemperatureUnits.css";
 
 export default function TemperatureUnits(props) {
-  const [unit, setUnit] = useState("celsius");
-
-  function convertFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function convertCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div className="TemperatureUnits">
-        <span className="degrees">{Math.round(props.celsius)}</span>
-        <span className="unit">
-          °C|{" "}
-          <a href="/" onClick={convertFahrenheit}>
-            °F
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    let fahrenheit = (props.celsius * 9) / 5 + 32;
-    return (
-      <div className="TemperatureUnits">
-        <span className="degrees">{Math.round(fahrenheit)}</span>
-        <span className="unit">
-          <a href="/" onClick={convertCelsius}>
-            °C
-          </a>
-          | °F
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="TemperatureUnits">
+      <span className="degrees">{Math.round(props.celsius)}</span>
+      <span className="unit">°C</span>
+    </div>
+  );
 }
